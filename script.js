2b8aae42496fb5074b2ba137e7c574c27e477a6e
Samuel Alves <alvesluizsamuel@gmail.com>
2026-05-04 23:46:58 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ window.addEventListener("scroll", () => {
   const header = document.getElementById("header");
   header.classList.toggle("scrolled", window.scrollY > 50);
 });
+
+// login.js
+function login() {
+  const role = document.querySelector("select").value;
+
+  localStorage.setItem("userRole", role);
+
+  if(role === "funcionario"){
+    alert("Entrou como FUNCIONÁRIO");
+  } else {
+    alert("Entrou como CLIENTE");
+  }
+}
